017b94f768f1a761e6786a7241d2070861486769
KalininaValeri <kalinina.valeri@gmail.com>
2017-04-07 19:02:46 +0700

    block about. style

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
-console.log('main');
+$(function () {
+    console.log(('main'));
+
+    var containerAbout = document.querySelector('.about');
+   
+   var addNewIcon = function () {
+       var div = document.createElement('div');
+       div.className = "about__icon about__icon_creative";
+       containerAbout.appendChild(div);
+   }
+
+    setTimeout(addNewIcon, 1000*10.3);
+   
+   
+});

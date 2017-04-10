bf5c9f79a387f6135fff790b798231e3a02c589b
KalininaValeri <kalinina.valeri@gmail.com>
2017-04-10 13:32:19 +0700

    block form. validation

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,15 @@ $(function () {
 
     var
         containerAbout = document.querySelector('.about'),
-        time = 0;
+        time = 0,
+        inputName = document.querySelector('.form-slide__input_name'),
+        inputPhone = document.querySelector('.form-slide__input_phone'),
+        btnSubmitForm = document.querySelector('.form-slide__btn');
 
-    var buldSodeSolushion = function(){
+    var buldSodeSolushion = function () {
         var
             nodeSolushion = document.querySelector('.about__icon_zadach');
-            nodeSolushionLeft = containerAbout.getBoundingClientRect().right - nodeSolushion.offsetWidth - 20,
+        nodeSolushionLeft = containerAbout.getBoundingClientRect().right - nodeSolushion.offsetWidth - 20,
             arrPoint = [];
 
         nodeSolushion.style.left = nodeSolushionLeft + 'px';
@@ -26,7 +29,7 @@ $(function () {
         }, timeAnimate);
     };
 
-    var addClassVisible = function(className, timeAdd) {
+    var addClassVisible = function (className, timeAdd) {
         setTimeout(function () {
             document.querySelector(className).classList.add('visible');
         }, timeAdd)
@@ -70,7 +73,7 @@ $(function () {
         document.querySelector('.logo').classList.remove('invisible');
     }, 1000 * 3.5 + 3000 + 3000 + 5000);
 
-    setInterval(function(){
+    setInterval(function () {
         arrPoint = document.querySelectorAll('.contact__point');
 
         var
@@ -90,10 +93,92 @@ $(function () {
         }
 
         each();
-    }, 1000 * 3.5 + 3000 + 3000 + 6000)
+    }, 1000 * 3.5 + 3000 + 3000 + 6000);
 
-    document.querySelector('.slider').addEventListener('click', function () {
+    // document.querySelector('.slider').addEventListener('click', function () {
+    //
+    //
+    //     document.querySelector('.slide.active').classList.add('prev');
+    //     document.querySelector('.slide.active').classList.remove('active');
+    //
+    //     document.querySelector('.slide.next').classList.add('active');
+    //     document.querySelector('.slide.next').classList.remove('next');
+    //
+    //     setTimeout(function () {
+    //         document.querySelector('.slide.prev').classList.add('next');
+    //         document.querySelector('.slide.prev').classList.remove('prev');
+    //     }, 1000);
+    //
+    //
+    //
+    // });
 
+    var validInputPhone = function (event) {
+
+        //enter only number
+        if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+            (event.keyCode == 65 && event.ctrlKey === true) ||
+            (event.keyCode == 187 && event.shiftKey === true) ||
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+            return;
+        } else {
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault();
+            }
+        }
+
+    };
+
+    inputPhone.addEventListener('keydown', validInputPhone);
+    inputPhone.addEventListener('focus', function () {
+
+        var str = inputPhone.value;
+
+        if (str.substring(0, 2) === '+7') {
+            str = str.replace(str.substring(0, 2), '');
+        }
+
+        inputPhone.value = '+7' + str;
+
+    });
+
+    var valid = function () {
+        var validNumber = 0;
+
+        if (inputName.value !== '') {
+            validNumber = validNumber + 1;
+        }
+
+        if (inputPhone.value.length >= 11) {
+            validNumber = validNumber + 1;
+        }
+
+        return validNumber;
+    };
+
+
+    document.querySelector('.form-slide.disable').addEventListener('keydown', function () {
+
+        var
+            inputValid = valid();
+
+        if (inputValid === 2) {
+            btnSubmitForm.classList.remove('disable');
+        }
+
+        if (inputValid !== 2) {
+            btnSubmitForm.classList.add('disable');
+        }
+
+    });
+
+    document.querySelector('.form-slide.disable').addEventListener('submit', function (e) {
+        e.preventDefault();
+        var validNumberSub = valid();
+
+        if (validNumberSub !== 2) {
+            return false;
+        }
 
         document.querySelector('.slide.active').classList.add('prev');
         document.querySelector('.slide.active').classList.remove('active');
@@ -106,13 +191,8 @@ $(function () {
             document.querySelector('.slide.prev').classList.remove('prev');
         }, 1000);
 
-
-
-        console.log('123');
+        console.log('submit');
     });
-
-
-
 
 
 });
